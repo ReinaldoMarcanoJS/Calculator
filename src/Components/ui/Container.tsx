8ca28a37c36db0e84eccf8b5dd29e49bfc653d79
Reinaldo Marcano <reynaldomarcano4@gmail.com>
2023-07-12 19:00:23 -0400

    Container

--- src/Components/ui/Container.tsx
+++ src/Components/ui/Container.tsx
@@ -8,8 +8,8 @@ export const Container: React.FC<Props> = ({ children, ThemeDark }) => {
     <div
       className={
         ThemeDark
-          ? "transition-colors duration-1000 w-screen h-screen bg-blue-main-background p-5 flex justify-center items-center "
-          : "transition-colors duration-1000 w-screen h-screen bg-Light-gray-main-background p-5 flex justify-center items-center "
+          ? "transition-colors duration-1000 w-screen h-screen bg-blue-main-background p-5 xs:pt-1 flex justify-center items-center "
+          : "transition-colors duration-1000 w-screen h-screen bg-Light-gray-main-background p-5 xs:pt-1 flex justify-center items-center "
       }>
       {children}
     </div>
--- src/Components/ui/DivToggle.tsx
+++ src/Components/ui/DivToggle.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 export const DivToggle: React.FC<Props> = ({ children}) => {
   return (
-    <div className="py-3 ">
+    <div className="pb-3 py-0 ">
           <div className="w-100% flex justify-between">
             {children}
           </div>
